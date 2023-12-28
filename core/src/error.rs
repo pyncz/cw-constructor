17afc8b2f8ebb780d19d95891a631e19e4658033
pyncz <pavelyanycz@gmail.com>
2023-12-28 18:42:49 +0300

    fix: Extend `ContractError` enum

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,8 +1,15 @@
-use cosmwasm_std::StdError;
+use cosmwasm_std::{Addr, StdError};
+use cw_utils::PaymentError;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("{sender} is not contract admin")]
+    Unauthorized { sender: Addr },
+
+    #[error("Payment error: {0}")]
+    Payment(#[from] PaymentError),
 }
