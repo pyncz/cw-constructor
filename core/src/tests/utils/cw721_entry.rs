ca48813bb30bdcf09b0baa06e19ba82a56cd18c1
pyncz <pavelyanycz@gmail.com>
2024-04-07 18:25:19 +0300

    test: Update test cw721

--- core/src/tests/utils/cw721_entry.rs
+++ core/src/tests/utils/cw721_entry.rs
@@ -1,36 +1,22 @@
+use cosmwasm_std::entry_point;
 use cosmwasm_std::Empty;
-use cw721::ContractInfoResponse;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 pub use cw721_base::{
     ContractError, Cw721Contract, ExecuteMsg, InstantiateMsg, MintMsg, MinterResponse, QueryMsg,
 };
+use serde::{Deserialize, Serialize};
 
 type Contract<'a, T> = Cw721Contract<'a, T, Empty>;
-
-#[cfg(not(feature = "library"))]
-use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-use serde::{Deserialize, Serialize};
 
 #[allow(dead_code)]
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate<T: Serialize + for<'de> Deserialize<'de> + Clone>(
     deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
+    env: Env,
+    info: MessageInfo,
     msg: InstantiateMsg,
 ) -> StdResult<Response> {
-    let contract = Contract::<T>::default();
-
-    let info = ContractInfoResponse {
-        name: msg.name,
-        symbol: msg.symbol,
-    };
-    contract.contract_info.save(deps.storage, &info)?;
-
-    let minter = deps.api.addr_validate(&msg.minter)?;
-    contract.minter.save(deps.storage, &minter)?;
-
-    Ok(Response::default())
+    Contract::<T>::default().instantiate(deps, env, info, msg)
 }
 
 #[allow(dead_code)]
