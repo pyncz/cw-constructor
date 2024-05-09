e47440014b6221a70c371d694fa21c20ec2121e3
pyncz <pavelyanycz@gmail.com>
2024-05-09 23:36:03 +0300

    build: Update scripts

---
+++ scripts/schema.sh
@@ -0,0 +1,14 @@
+# Core
+echo "> core"
+cargo run --manifest-path ./core/Cargo.toml schema;
+
+# Demo contracts
+for dir in ./example/contracts/*/
+do
+  dir=${dir%*/}
+  dirname=${dir##*/}
+  if [ $dirname != shared ]; then
+    echo "\n> ${dirname}";
+    cargo run --manifest-path ${dir}/Cargo.toml schema;
+  fi
+done
