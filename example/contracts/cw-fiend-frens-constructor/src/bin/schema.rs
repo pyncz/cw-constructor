b53cdc04084c99c9dae53df8c2cea942927d0cb1
pyncz <pavelyanycz@gmail.com>
2024-06-14 02:19:34 +0300

    chore: Setup `migrate` entrypoint for `cw-fiend-frens-constructor` contract

--- example/contracts/cw-fiend-frens-constructor/src/bin/schema.rs
+++ example/contracts/cw-fiend-frens-constructor/src/bin/schema.rs
@@ -1,10 +1,12 @@
 use cosmwasm_schema::write_api;
+use cosmwasm_std::Empty;
 use cw_constructor::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
         execute: ExecuteMsg,
-        query: QueryMsg
+        query: QueryMsg,
+        migrate: Empty
     }
 }
--- example/contracts/cw-fiend-frens-constructor/src/lib.rs
+++ example/contracts/cw-fiend-frens-constructor/src/lib.rs
@@ -2,6 +2,7 @@ use cw_constructor::contract::Contract as ConstructorContract;
 use cw_constructor::error::ContractResponse;
 use cw_constructor::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use cw_fiend_frens_shared::metadata::{Extension, MergedExtension, TraitExtension};
+use semver::Version;
 
 // Version info for migration
 const CONTRACT_NAME: &str = "fiend-frens-constructor";
@@ -14,7 +15,8 @@ pub mod entry {
 
     #[cfg(not(feature = "library"))]
     use cosmwasm_std::entry_point;
-    use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, StdResult};
+    use cosmwasm_std::{Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult};
+    use cw_constructor::error::ContractError;
 
     #[cfg_attr(not(feature = "library"), entry_point)]
     pub fn instantiate(
@@ -44,5 +46,36 @@ pub mod entry {
     pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
         let contract = Contract::default();
         contract.query(deps, env, msg)
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn migrate(deps: DepsMut, _env: Env, _msg: Empty) -> ContractResponse {
+        // Current contract state
+        let current = cw2::get_contract_version(deps.storage)?;
+        let original_name = current.contract;
+        let current_version = current.version;
+        let current_version_semver: Version = current_version.parse()?;
+
+        // New state to migrate to
+        let new_name = CONTRACT_NAME.to_string();
+        let new_version: String = CONTRACT_VERSION.to_string();
+        let new_version_semver: Version = new_version.parse()?;
+
+        // Validate migration params
+        if original_name != new_name {
+            return Err(ContractError::InvalidMigrationContractName {
+                original_name,
+                new_name,
+            });
+        }
+        if current_version_semver >= new_version_semver {
+            return Err(ContractError::InvalidMigrationVersion {
+                current_version,
+                new_version,
+            });
+        }
+
+        cw2::set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+        Ok(Response::default())
     }
 }
